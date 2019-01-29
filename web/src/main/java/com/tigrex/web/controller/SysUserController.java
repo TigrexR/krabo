449bef62a40696ad23f01dbc3aa3fb91f82502bb
TigrexR <swcdasd@sina.com>
2019-01-29 17:22:13 +0800

    mybatis-plus

--- web/src/main/java/com/tigrex/web/controller/SysUserController.java
+++ web/src/main/java/com/tigrex/web/controller/SysUserController.java
@@ -1,6 +1,8 @@
 package com.tigrex.web.controller;
 
 import com.alibaba.dubbo.config.annotation.Reference;
+import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
+import com.tigrex.admin.entity.SysUser;
 import com.tigrex.admin.service.SysUserService;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -11,5 +13,13 @@ public class SysUserController {
 
     @Reference(version = "1.0.0")
     private SysUserService sysUserService;
+
+    @RequestMapping(value = "getSysUserInfo")
+    public SysUser getSysUserInfo(){
+        QueryWrapper wrapper = new QueryWrapper();
+        wrapper.eq("id", 1);
+        SysUser sysUser = sysUserService.getOne(wrapper);
+        return sysUser;
+    }
 
 }
